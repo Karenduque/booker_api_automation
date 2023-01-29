99644f16b6cbe9ce8697d4077d8608a70f2837e6
Karen Alexandra Duque Solarte <karenalexandra.duque@gmail.com>
2023-01-28 21:47:56 -0500

    Update Utilities.java
    se agrega utilidad id random

--- src/test/java/com/booker/testing/utilities/commons/Utilities.java
+++ src/test/java/com/booker/testing/utilities/commons/Utilities.java
@@ -2,4 +2,8 @@ package com.booker.testing.utilities.commons;
 
 public class Utilities {
 
+     public static String getIdRandom(){
+        String result = java.util.UUID.randomUUID().toString();
+        return result;
+    }
 }
